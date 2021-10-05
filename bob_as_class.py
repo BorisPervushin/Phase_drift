81f684aa009add24cba2d52f59c346884f2d4269
Boris Pervushin <borispervushin0@gmail.com>
2021-10-05 16:25:48 +0300

    Working version with ampl phase

--- bob_as_class.py
+++ bob_as_class.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
-import json
 
 
 def file_transform(old_filename: str, new_filename: str, start_index=4675, sample_count=200000) -> None:
@@ -17,7 +16,6 @@ def file_transform(old_filename: str, new_filename: str, start_index=4675, sampl
 
 
 class Bob:
-    # TODO: Добавить описание
     # TODO: написание тестов для существующих данных
     # TODO: Использование статистики
     # TODO: Доработка ПО для работы с импульсным режимом
@@ -35,7 +33,9 @@ class Bob:
                  adc_resolution=12,
                  ref_sig_quantity=(2, 4),
                  min_indexes_in_pulse=200,
-                 max_indexes_in_pulse=2000):
+                 max_indexes_in_pulse=2000,
+                 step_between_ref_and_sig=1):
+        self.step_between_ref_and_sig = step_between_ref_and_sig
         self.global_index = global_index  # индекс начала текущего цикла
         self.state_table = {'Pulse type': [], 'Quadrature': [], 'Value': [], 'Bits': []}  # итоговая таблица
         self.statistics_ref = [[0, 0] for i in range(ref_sig_quantity[0])]  # массив статистики ипульсов в референсе:
@@ -67,19 +67,22 @@ class Bob:
         self.samples = np.array([
             np.array(pd.read_csv(data_filename, dtype={"Time": float, "Amplitude": float})['Time']),
             np.array(pd.read_csv(data_filename, dtype={"Time": float, "Amplitude": float})['Amplitude'])
-        ])
-        self.min_indexes_in_pulse = min_indexes_in_pulse
-        self.max_indexes_in_pulse = max_indexes_in_pulse
-        self.ref_len_in_indexes_stats = 0
+        ])  # ndarray, 1 массив - значения по x, 2 массив - значения по y
+        self.min_indexes_in_pulse = min_indexes_in_pulse  # минимальное количество точек в импульсе (на глаз)
+        # подбирается для адекватного рассчета СКО референсного импульса
+        self.max_indexes_in_pulse = max_indexes_in_pulse  # максимальное количество точек в
+        # в импульсе (на глаз) подбирается для ограничения сдвига левой границы
+        self.ref_len_in_indexes_stats = 0  # статистическое значение длины всех референсных импульсов
 
     def bob_adc(self, voltage_value: float) -> str:
         """Оцифровка: возвращение битовой последовательности"""
         # step = voltage_range / (2 ** resolution)
-        voltage_range = self.adc_resolution
-        resolution = self.adc_resolution
-        grid = np.linspace(-voltage_range / 2, voltage_range / 2, 2 ** resolution - 1)
+        voltage_range = self.adc_voltage_range  # V p-p
+        resolution = self.adc_resolution  # битность АЦП
+        grid = np.linspace(-voltage_range / 2, voltage_range / 2, 2 ** resolution - 1)  # сетка АЦП
         left = 0
         right = len(grid)
+        # Организация бинарного поиска
         if voltage_value < grid[0]:
             return '0' * resolution
         while right > left + 1:
@@ -137,9 +140,10 @@ class Bob:
                 checking_std = self.max_std
                 """"Цикл поиска референсных импульсов"""
                 # indexes_for_ref[j].append(i_pulse_start)
-                i = i_pulse_start + self.min_indexes_in_pulse
+                i = i_pulse_start + int(self.min_indexes_in_pulse * 0.5)
                 checking_std = max(self.max_std, float(np.std(self.samples[1][i_pulse_start:i - 1])))
-                while abs(self.samples[1][i] - np.mean(self.samples[1][i_pulse_start:i])) <= checking_std * 5:
+                while abs(self.samples[1][i] - np.mean(self.samples[1][
+                                                       i_pulse_start:i])) <= checking_std * 5 and i < i_pulse_start + self.max_indexes_in_pulse * 1.5:
                     # проверка выхода за границы пяти СКО от среднего значения по значениям пмплитуд
                     checking_std = max(self.max_std, float(np.std(self.samples[1][i_pulse_start:i - 1])))
                     i += 1
@@ -151,7 +155,7 @@ class Bob:
                 while abs(self.samples[1][i_pulse_start - 1] -
                           np.mean(self.samples[1][i_pulse_start:i + 1])) < checking_std * 5 \
                         and i_pulse_start >= 1 \
-                        and abs(i_pulse_start - i_left) <= self.max_indexes_in_pulse:
+                        and abs(i_pulse_start - i_left) <= self.max_indexes_in_pulse / 2:
                     """Сдвиг левой границы"""
                     i_pulse_start -= 1
                     checking_std = max(self.max_std, float(np.std(self.samples[1][i_pulse_start:i - 1])))
@@ -175,10 +179,11 @@ class Bob:
                 for index_next_pulse in range(i + 1, self.max_indexes_in_pulse):
                     # цикл по точкам после найденного импульса для определения точки следующего импульса
                     if abs(self.samples[1][index_next_pulse] - self.samples[1][index_next_pulse + 5]) < pulse_std * 5:
-                        # условие, что разница амплитуд двух последовательных точек будет меньше 5 СКО предыдущего импульса
+                        # условие, что разница амплитуд двух последовательных
+                        # точек будет меньше 5 СКО предыдущего импульса
                         break
-                index_next_pulse += 4
-                i_pulse_start = index_next_pulse + 4
+                index_next_pulse += self.step_between_ref_and_sig
+                i_pulse_start = index_next_pulse + self.step_between_ref_and_sig
             indexes_for_ref[-1].append(index_next_pulse)
 
             indexes_for_ref[-2] = max(
@@ -199,11 +204,10 @@ class Bob:
                             self.cycle_number / (self.cycle_number + 1)) + ref_width_for_statistic / (
                             self.cycle_number + 1)
                 ]
-
+        print(indexes_for_ref)
         return indexes_for_ref
 
     def signal_pulse_definition(self, ref_info: list) -> list:
-        samples = self.samples
         pulse_values = []
         for signal_number in range(round(self.ref_sig_quantity[1] / self.ref_sig_quantity[0])):
             index_shift = signal_number * (ref_info[-1][1] - ref_info[-1][0])
@@ -213,7 +217,7 @@ class Bob:
             #    print(center - floor(pulse_width[count] / 2), center + floor(pulse_width[count] / 2) , 4)
             pulse_values += [
                 np.mean(
-                    samples[1][center - mt.floor(pulse_width[count] / 2): center + mt.floor(pulse_width[count] / 2)])
+                    self.samples[1][int(center) - mt.floor(pulse_width[count] / 2): int(center) + mt.floor(pulse_width[count] / 2)])
                 for count, center in enumerate(pulse_centers)]
             self.sig_left_indexes += [(pulse_centers[i] - mt.floor(pulse_width[i] / 2))
                                       for i in range(len(pulse_centers))]
@@ -251,7 +255,7 @@ class Bob:
 
 class PlottingSignal:
     def __init__(self, start, stop, data_filename,
-                 *indexes_for_dot_plotting, ):
+                 *indexes_for_dot_plotting):
 
         self.samples = np.array([
             np.array(pd.read_csv(data_filename, dtype={"Time": float, "Amplitude": float})['Time']),
@@ -259,6 +263,8 @@ class PlottingSignal:
         ])
         self.samples = np.array([[(item - self.samples[0][0]) / (self.samples[0][2] - self.samples[0][1])
                                   for item in self.samples[0]], self.samples[1]])
+        if stop == 'max':
+            stop = max(np.shape(self.samples))
         plt.plot(self.samples[0][start:stop], self.samples[1][start:stop])
         for item, color in indexes_for_dot_plotting:
             # colors = [color for color in mcolors.CSS4_COLORS.keys()]
@@ -270,18 +276,31 @@ class PlottingSignal:
 
 
 if __name__ == '__main__':
-    bob = Bob(data_filename='Ampl modul/amp and phase o-pi2 transformed.csv', total_cycle_number=10)
+    # oscil_filename = 'Ampl modul/2 bob pulse 0-pi2_alice 0 transformed.csv'
+    oscil_filename = 'Ampl modul/amp and phase o-pi2 transformed.csv'
+    # file_transform(
+    #     old_filename='Ampl modul/2 bob pulse 0-pi2_alice 0.csv',
+    #     new_filename=oscil_filename,
+    #     start_index=10)
+    #
+    # plot_only_osc = PlottingSignal(start=0, stop=1000, data_filename=oscil_filename)
+
+    bob = Bob(data_filename=oscil_filename,
+              total_cycle_number=150,
+              step_between_ref_and_sig=4)
+              # max_indexes_in_pulse=50,
+              # min_indexes_in_pulse=5)
     # ref1 = bob.ref_pulse_definition()
     for i in range(bob.total_cycle_number):
-        bob.make_cycle()
+        bob.make_cycle()  # реализация цикла: определение референсных импульсов, получение значений сигнальных импульсов
     # sig1 = bob.signal_pulse_definition(ref1)
 
     df = pd.DataFrame(bob.state_table)
-    df.to_csv('Bob class table.csv')
+    df.to_csv('CSV files/Bob class table.csv')
 
     plot = PlottingSignal(0, max(bob.ref_right_indexes[-1], bob.sig_right_indexes[-1]) + 500,
-                          'Ampl modul/amp and phase o-pi2 transformed.csv',
+                          oscil_filename,(bob.ref_mid_indexes, 'coral'),
                           (bob.sig_right_indexes, 'red'),
                           (bob.sig_mid_indexes, 'green'),
-                          (bob.sig_left_indexes, 'blue'),
-                          (bob.ref_mid_indexes, 'coral'))
+                          (bob.sig_left_indexes, 'blue'))
+
